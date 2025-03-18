2e12b0099650484f7ee07529ba012eeba58008e8
Kyra4code <lucarioecharmito@gmail.com>
2025-03-18 18:09:32 -0300

    Atualizado um cors no back para que o front acesse

--- main.py
+++ main.py
@@ -3,8 +3,20 @@ from pydantic import BaseModel
 from database.Get.getUser import verUser
 from nutrIA.Ia import read_root;
 import os
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+app = FastAPI();
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+#Isso aqui permite que o front-end acesse o back-end.
+
 
 class Pergunta(BaseModel):
     pergunta: str
