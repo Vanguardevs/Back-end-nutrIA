d5a7c5c3b8300232ecbcc47a48ddaa9ea7c403d3
Vitor Guilherme R. <162996534+Kyra4code@users.noreply.github.com>
2025-04-14 22:08:58 -0300

    Limitação da IA

--- nutrIA/Ia.py
+++ nutrIA/Ia.py
@@ -11,12 +11,12 @@ class Pergunta(BaseModel):
 API_KEY = os.getenv("GEMINI_API")
 gemini.configure(api_key=API_KEY);
 meta = "ficar musculoso"
-model = gemini.GenerativeModel("gemini-1.5-flash", system_instruction=f"Você é uma assistente nutricional de um aplicativo chamado NutrIA e esse é seu nome e você auxiliará o usuário com base na meta de {meta}, apenas isso.")
+model = gemini.GenerativeModel("gemini-1.5-flash", system_instruction=f"Você é uma assistente nutricional de um aplicativo chamado NutrIA e esse é seu nome. Você auxiliará o usuário e terá que ser simples e direta. Apenas isso.")
 # Ia = modelo.generate_content("Qual dia de hoje?")
 chat = model.start_chat(history=[])
 
 chat = model.start_chat(history=[])
 
 async def read_root(question: Pergunta):
-    resposta = await chat.send_message_async(question.pergunta, generation_config=gemini.GenerationConfig(max_output_tokens=150,temperature=0.2))
+    resposta = await chat.send_message_async(question.pergunta, generation_config=gemini.GenerationConfig(max_output_tokens=100,temperature=0.1))
     return {"pergunta": question.pergunta, "resposta": resposta.text}
