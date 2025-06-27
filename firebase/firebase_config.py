786d7131dfa6da815817a444f9a67a8e1552a1e6
Vitor Guilherme R. <162996534+Kyra4code@users.noreply.github.com>
2025-06-26 22:18:48 -0300

    Update firebase_config.py

--- firebase/firebase_config.py
+++ firebase/firebase_config.py
@@ -3,8 +3,8 @@ from firebase_admin import credentials, db
 
 # Verifica se o Firebase já foi inicializado
 if not firebase_admin._apps:
-    # cred = credentials.Certificate("firebase/nutria.json")
-    cred = credentials.Certificate("/etc/secrets/nutria.json")
+    cred = credentials.Certificate("firebase/nutria.json")
+    # cred = credentials.Certificate("/etc/secrets/nutria.json")
     firebase_admin.initialize_app(cred, {
         'databaseURL': 'https://nutria-eafaa-default-rtdb.firebaseio.com/'
     })
